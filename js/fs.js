1eb3c95c9e9a297b89fcf058ff46c25a476c5964
One_Random <One_Random@outlook.com>
2020-08-13 01:27:29 +0800

    based on unix

--- js/fs.js
+++ js/fs.js
@@ -2,49 +2,40 @@
  * @Author: One_Random
  * @Date: 2020-08-13 00:08:42
  * @LastEditors: One_Random
- * @LastEditTime: 2020-08-13 00:41:59
- * @FilePath: /HTML/OS/FS/js/fs.js
+ * @LastEditTime: 2020-08-13 01:27:13
+ * @FilePath: /FS/js/fs.js
  * @Description: Copyright © 2020 One_Random. All rights reserved.
- */ 
+ */
+
+/*
+ * 用户组的类
+ */
+class User_Group {
+    constructor() {
+        this.ID;
+        this.name;
+    }
+}
 
 /*
  * 用户的类
  */
 class User {
-    constructor(){
-        this.userID = null;   // ID
-        this.username = null; // 名称
+    constructor() {
+        this.ID = null;   // ID
+        this.name = null; // 名称
         // this.password = null; // 密码
 
-        this.type = null;     // 类别
-        this.root = null;     // 根文件夹
+        this.groupID = null;     // 用户组
     }
 }
 
 /*
- * 文件夹的类
+ *  基础存储的类
  */
-class Folder {
-    constructor(){
-        this.name = null;   // 名称
-        this.size = null;   // 大小
-        this.created_time;  // 创建时间
-        //this.modified_time; // 修改时间
-        //this.last_open_time; // 上次打开时间
-        this.comments;      // 描述
-        this.permissions; // 权限管理
-
-        this.parent = null; // 父文件夹
-        this.folders = null;   // 子文件夹
-        this.files = null;  // 子文件
-    }
-}
-
-/*
- * 文件的类
- */
-class File {
-    constructor(){
+class Binary {
+    constructor() {
+        this.ID;            // 标识
         this.name = null;   // 名称
         this.extension;     // 扩展名
         this.size = null;   // 大小
@@ -53,8 +44,31 @@ class File {
         //this.last_open_time; // 上次打开时间
         this.comments;      // 描述
         this.permissions; // 权限管理
+    }
+}
+
+/*
+ * 文件的类
+ */
+class File extends Binary {
+    constructor() {
+        super();
+        this.executable;
 
         this.parent = null; // 父文件夹
+    }
+}
+
+/*
+ * 文件夹的类
+ */
+class Folder extends Binary {
+    constructor() {
+        super(); // 继承自Binary类
+
+        this.parent = null; // 父文件夹
+        this.folders = null;   // 子文件夹
+        this.files = null;  // 子文件
     }
 }
 
@@ -63,15 +77,15 @@ class File {
  */
 class Privilege {
     constructor() {
-        this.CANTREAD = -1;
-        this.READONLY = 0;
-        this.READWRITE = 1;
+        this.EXECUTE = 1;
+        this.WRITE = 2;
+        this.READ = 4;
     }
 }
 
 class Permission {
     constructor() {
-        this.name;
+        this.name; // user group other all
         this.privilege;
     }
 }
@@ -81,6 +95,9 @@ class Permission {
  */
 class System {
     constructor() {
+        this.FILE = 0;
+        this.DIR = 1;
+
         this.users = new Array();
     }
 }
@@ -92,6 +109,5 @@ class Shell {
     constructor() {
         this.user;
         this.dir;
-        
     }
 }
